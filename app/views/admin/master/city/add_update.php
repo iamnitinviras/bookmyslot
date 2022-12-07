0b30af606fcba493c98ebf372643453d27232a47
Nitin Viras <nitinboricha91@gmail.com>
2022-12-07 14:51:21 +0530

    update

--- app/views/admin/master/city/add_update.php
+++ app/views/admin/master/city/add_update.php
@@ -2,37 +2,45 @@
 if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') {
     include VIEWPATH . 'vendor/header.php';
     $folder_name = 'vendor';
+    $form_url = 'vendor/save-city';
 } else {
     include VIEWPATH . 'admin/header.php';
     $folder_name = 'admin';
+    $form_url = 'admin/save-city';
 }
+
 $city_title = (set_value("city_title")) ? set_value("city_title") : (!empty($city_data) ? $city_data['city_title'] : '');
 $city_status = (set_value("city_status")) ? set_value("city_status") : (!empty($city_data) ? $city_data['city_status'] : '');
 $id = (set_value("city_id")) ? set_value("city_id") : (!empty($city_data) ? $city_data['city_id'] : 0);
 ?>
 <input id="folder_name" name="folder_name" type="hidden" value="<?php echo isset($folder_name) && $folder_name != '' ? $folder_name : ''; ?>"/>
-<div class="dashboard-body">
-    <!-- Start Content -->
-    <div class="content">
-        <!-- Start Container -->
-        <div class="container-fluid">
-            <section class="form-light px-2 sm-margin-b-20 ">
-                <?php $this->load->view('message'); ?>
 
-                <div class="card mt-4">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
                     <div class="card-header">
-                        <h5 class="black-text font-bold mb-0">
-                            <?php echo isset($id) && $id > 0 ? translate('update') : translate('add'); ?> <?php echo translate('city'); ?>
-                        </h5>
+                        <div class="row">
+                            <div class="col-md-6 col-xl-6">
+                                <h4 class="card-title"><?php echo isset($id) && $id > 0 ? translate('update') : translate('add'); ?> <?php echo translate('city'); ?></h4>
+                                <div class="page-title-box pb-0 d-sm-flex">
+                                    <div class="page-title-right">
+                                        <ol class="breadcrumb m-0">
+                                            <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>"><?php echo translate('dashboard'); ?></a></li>
+                                            <li class="breadcrumb-item"><a href="<?php echo base_url('admin/city'); ?>"<?php echo translate('city'); ?></a></li>
+                                            <li class="breadcrumb-item active"><?php echo isset($id) && $id > 0 ? translate('update') : translate('add'); ?> <?php echo translate('city'); ?></li>
+                                        </ol>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-body resp_mx-0">
-                        <?php
-                        if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') {
-                            $form_url = 'vendor/save-city';
-                        } else {
-                            $form_url = 'admin/save-city';
-                        }
-                        ?>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-12 m-auto">
+                                <?php $this->load->view('message'); ?>
+                            </div>
+                        </div>
                         <?php
                         echo form_open($form_url, array('name' => 'CityForm', 'id' => 'CityForm'));
                         echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $id));
@@ -41,7 +49,7 @@ $id = (set_value("city_id")) ? set_value("city_id") : (!empty($city_data) ? $cit
                             <div class="col-md-4">
                                 <div class="form-group">
                                     <label for="city_title"> <?php echo translate('title'); ?><small class="required">*</small></label>
-                                    <input type="text" autocomplete="off" id="city_title" name="city_title" value="<?php echo $city_title; ?>" class="form-control" placeholder="<?php echo translate('title'); ?>">                                    
+                                    <input type="text" autocomplete="off" id="city_title" name="city_title" value="<?php echo $city_title; ?>" class="form-control" placeholder="<?php echo translate('title'); ?>">
                                     <?php echo form_error('city_title'); ?>
                                 </div>
                             </div>
@@ -75,14 +83,10 @@ $id = (set_value("city_id")) ? set_value("city_id") : (!empty($city_data) ? $cit
                         </div>
                         <?php echo form_close(); ?>
                     </div>
-                    <!--/Form with header-->
                 </div>
-                <!--Card-->
-            </section>
-            <!-- End Login-->
+            </div>
         </div>
     </div>
-</div>
 <script src="<?php echo $this->config->item('js_url'); ?>module/city.js" type="text/javascript"></script>
 <?php
 if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') {
--- app/views/admin/master/city/index.php
+++ app/views/admin/master/city/index.php
@@ -8,104 +8,106 @@ if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') 
 }
 ?>
 <input id="folder_name" name="folder_name" type="hidden" value="<?php echo isset($folder_name) && $folder_name != '' ? $folder_name : ''; ?>"/>
-<div class="dashboard-body">
-    <!-- Start Content -->
-    <div class="content">
-        <!-- Start Container -->
-        <div class="container-fluid ">
-            <section class="form-light px-2 sm-margin-b-20">
-                <!-- Row -->
-                <div class="row">
-                    <div class="col-md-12 m-auto">
-                        <?php $this->load->view('message'); ?>
-
-                        <div class="card mt-4">
-                            <div class="card-header">
-                                <div class="row">
-                                <span class="col-md-9 col-9 m-0">
-                                    <h5 class="black-text font-bold mb-0"><?php echo translate('manage'); ?> <?php echo translate('city'); ?></h5>
-                                </span>
-                                    <span class="col-md-3 col-3 text-right m-0">
-                                    <?php if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') { ?>
-                                        <a  href='<?php echo base_url('vendor/add-city'); ?>' class="btn btn-outline-success"><i class="fa fa-plus-circle"></i></a>
-                                    <?php } else { ?>
-                                        <a  href='<?php echo base_url('admin/add-city'); ?>' class="btn btn-outline-success"><i class="fa fa-plus-circle"></i></a>
-                                    <?php } ?>
-                                </span>
-                                </div>
-                            </div>
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table mdl-data-table" id="example">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-center font-bold dark-grey-text">#</th>
-                                                <th class="text-center font-bold dark-grey-text"><?php echo translate('title'); ?></th>
-                                                <th class="text-center font-bold dark-grey-text"><?php echo translate('status'); ?></th>
-                                                <?php if ($folder_name == 'admin'): ?>
-                                                    <th class="text-center font-bold dark-grey-text"><?php echo translate('is_default'); ?></th>
-                                                <?php endif; ?>
-                                                <th class="text-center font-bold dark-grey-text"><?php echo translate('created_date'); ?></th>
-                                                <th class="text-center font-bold dark-grey-text"><?php echo translate('action'); ?></th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            <?php
-                                            if (isset($city_data) && count($city_data) > 0) {
-                                                foreach ($city_data as $key => $row) {
-                                                    if ($row['city_status'] == "A") {
-                                                        $status_string = '<span class="badge badge-success">' . translate('active') . '</span>';
-                                                    } else {
-                                                        $status_string = '<span class="badge badge-danger">' . translate('inactive') . '</span>';
-                                                    }
-                                                    ?>
-                                                    <tr>
-                                                        <td class="text-center"><?php echo $key + 1; ?></td>
-                                                        <td class="text-center"><?php echo $row['city_title']; ?></td>
-                                                        <td class="text-center"><?php echo $status_string; ?></td>
-                                                        <?php if ($folder_name == 'admin'): ?>
-                                                            <td class="text-center">
-                                                                <input type="checkbox" <?php echo ($row['is_default'] == 1) ? "checked='checked'" : ""; ?> style="visibility: visible !important;left: 0px !important;position: relative;" name="is_default" data-id="<?php echo $row['city_id']; ?>"/>
-                                                            </td>
-                                                        <?php endif; ?>
-
-                                                        <td class="text-center"><?php echo get_formated_date($row['city_created_on'], "N"); ?></td>
-                                                        <td class="td-actions text-center">
-                                                            <?php
-                                                            if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') {
-                                                                $created_by = $this->session->userdata('Vendor_ID');
-                                                                if ($created_by == $row['city_created_by']) {
-                                                                    ?>
-                                                                    <a href="<?php echo base_url('vendor/update-city/' . $row['city_id']); ?>" class="btn btn-primary font_size_12" title="<?php echo translate('edit'); ?>"><i class="fa fa-pencil"></i></a>
-                                                                    <a id="" data-toggle="modal" onclick='DeleteRecord(this)' data-target="#delete-record" data-id="<?php echo (int) $row['city_id']; ?>" class="btn btn-danger font_size_12" title="<?php echo translate('delete'); ?>"><i class="fa fa-trash"></i></a>
-                                                                    <?php
-                                                                } else {
-                                                                    echo '-';
-                                                                }
-                                                            } else {
-                                                                ?>
-                                                                <a href="<?php echo base_url('admin/update-city/' . $row['city_id']); ?>" class="btn btn-primary font_size_12" title="<?php echo translate('edit'); ?>" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil"></i></a>
-                                                                <span class="d-inline-block" title="<?php echo translate('delete'); ?>" data-toggle="tooltip" data-placement="top"><a id="" data-toggle="modal" onclick='DeleteRecord(this)' data-target="#delete-record" data-id="<?php echo (int) $row['city_id']; ?>" class="btn btn-danger font_size_12"><i class="fa fa-trash"></i></a></span>
-                                                            <?php } ?>
-                                                        </td>
-                                                    </tr>
-                                                    <?php
-                                                }
-                                            }
-                                            ?>
-                                        </tbody>
-                                    </table>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-6">
+                            <h4 class="card-title"><?php echo translate('manage'); ?> <?php echo translate('city'); ?></h4>
+                            <div class="page-title-box pb-0 d-sm-flex">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>"><?php echo translate('dashboard'); ?></a></li>
+                                        <li class="breadcrumb-item active"><?php echo translate('city'); ?></li>
+                                    </ol>
                                 </div>
                             </div>
                         </div>
+                        <div class="col-md-6 col-xl-6 text-end add-new-btn-parent">
+                            <div class="text-sm-end">
+                                <a href='<?php echo base_url($folder_name.'/add-city'); ?>'class="btn btn-success btn-rounded waves-effect waves-light mb-2 me-2"><i class="mdi mdi-plus me-1"></i> <?php echo translate('add'); ?> <?php echo translate('city'); ?></a>
+                            </div>
+                        </div>
                     </div>
-                    <!--col-md-12-->
                 </div>
-                <!--Row-->
-            </section>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 m-auto">
+                            <?php $this->load->view('message'); ?>
+
+                            <div class="table-responsive">
+                                <table class="table mdl-data-table" id="example">
+                                    <thead>
+                                    <tr>
+                                        <th class="text-center font-bold dark-grey-text">#</th>
+                                        <th class="text-center font-bold dark-grey-text"><?php echo translate('title'); ?></th>
+                                        <th class="text-center font-bold dark-grey-text"><?php echo translate('status'); ?></th>
+                                        <?php if ($folder_name == 'admin'): ?>
+                                            <th class="text-center font-bold dark-grey-text"><?php echo translate('is_default'); ?></th>
+                                        <?php endif; ?>
+                                        <th class="text-center font-bold dark-grey-text"><?php echo translate('created_date'); ?></th>
+                                        <th class="text-center font-bold dark-grey-text"><?php echo translate('action'); ?></th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <?php
+                                    if (isset($city_data) && count($city_data) > 0) {
+                                        foreach ($city_data as $key => $row) {
+                                            if ($row['city_status'] == "A") {
+                                                $status_string = '<span class="badge badge-success">' . translate('active') . '</span>';
+                                            } else {
+                                                $status_string = '<span class="badge badge-danger">' . translate('inactive') . '</span>';
+                                            }
+                                            ?>
+                                            <tr>
+                                                <td class="text-center"><?php echo $key + 1; ?></td>
+                                                <td class="text-center"><?php echo $row['city_title']; ?></td>
+                                                <td class="text-center"><?php echo $status_string; ?></td>
+                                                <?php if ($folder_name == 'admin'): ?>
+                                                    <td class="text-center">
+                                                        <input type="checkbox" <?php echo ($row['is_default'] == 1) ? "checked='checked'" : ""; ?> style="visibility: visible !important;left: 0px !important;position: relative;" name="is_default" data-id="<?php echo $row['city_id']; ?>"/>
+                                                    </td>
+                                                <?php endif; ?>
+
+                                                <td class="text-center"><?php echo get_formated_date($row['city_created_on'], "N"); ?></td>
+                                                <td class="td-actions text-center">
+                                                    <?php
+                                                    if ($this->session->userdata('Type_' . ucfirst($this->uri->segment(1))) == 'V') {
+                                                        $created_by = $this->session->userdata('Vendor_ID');
+                                                        if ($created_by == $row['city_created_by']) {
+                                                            ?>
+                                                            <a href="<?php echo base_url('vendor/update-city/' . $row['city_id']); ?>" class="btn btn-primary font_size_12" title="<?php echo translate('edit'); ?>"><i class="fa fa-pencil"></i></a>
+                                                            <a id="" data-toggle="modal" onclick='DeleteRecord(this)' data-target="#delete-record" data-id="<?php echo (int) $row['city_id']; ?>" class="btn btn-danger font_size_12" title="<?php echo translate('delete'); ?>"><i class="fa fa-trash"></i></a>
+                                                            <?php
+                                                        } else {
+                                                            echo '-';
+                                                        }
+                                                    } else {
+                                                        ?>
+                                                        <a href="<?php echo base_url('admin/update-city/' . $row['city_id']); ?>" class="btn btn-primary font_size_12" title="<?php echo translate('edit'); ?>" data-toggle="tooltip" data-placement="top"><i class="fa fa-pencil"></i></a>
+                                                        <span class="d-inline-block" title="<?php echo translate('delete'); ?>" data-toggle="tooltip" data-placement="top"><a id="" data-toggle="modal" onclick='DeleteRecord(this)' data-target="#delete-record" data-id="<?php echo (int) $row['city_id']; ?>" class="btn btn-danger font_size_12"><i class="fa fa-trash"></i></a></span>
+                                                    <?php } ?>
+                                                </td>
+                                            </tr>
+                                            <?php
+                                        }
+                                    }
+                                    ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!--col-md-12-->
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>   
+    </div>
+    <!-- end row -->
 </div>
+
 <!-- Modal -->
 <div class="modal fade" id="delete-record">
     <div class="modal-dialog">
