81698ed23f995a2854c3e0f7084d5497c2742d6f
Nitin Viras <nitinboricha91@gmail.com>
2022-12-14 08:48:09 +0530

    update

--- app/views/admin/header.php
+++ app/views/admin/header.php
@@ -94,7 +94,7 @@
 
             <div class="d-flex">
 
-                <div class="dropdown d-none d-lg-inline-block ms-1">
+                <div class="dropdown d-inline-block">
                     <button type="button" class="btn header-item"
                             data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                         <i data-feather="grid" class="icon-lg"></i>
@@ -102,135 +102,47 @@
                     <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end">
                         <div class="p-2">
                             <div class="row g-0">
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
+                                <div class="col-md-4">
+                                    <a class="dropdown-icon-item" href="<?php echo base_url('admin/city'); ?>">
                                         <img src="<?php echo base_url('assets/admin/images/brands/github.png');?>" alt="Github">
-                                        <span>GitHub</span>
+                                        <span><?php echo translate('city') ?></span>
                                     </a>
                                 </div>
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
+                                <div class="col-md-4">
+                                    <a class="dropdown-icon-item" href="<?php echo base_url('admin/location'); ?>">
                                         <img src="<?php echo base_url('assets/admin/images/brands/bitbucket.png');?>" alt="bitbucket">
-                                        <span>Bitbucket</span>
+                                        <span><?php echo translate('location') ?></span>
                                     </a>
                                 </div>
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
+                                <div class="col-md-4">
+                                    <a class="dropdown-icon-item" href="<?php echo base_url('admin/currency'); ?>">
                                         <img src="<?php echo base_url('assets/admin/images/brands/dribbble.png');?>" alt="dribbble">
-                                        <span>Dribbble</span>
+                                        <span><?php echo translate('currency'); ?></span>
+                                    </a>
+                                </div>
+
+                                <?php if (get_site_setting('enable_testimonial') == 'Y'): ?>
+                                    <div class="col-md-4">
+                                        <a class="dropdown-icon-item" href="<?php echo base_url('admin/testimonial'); ?>">
+                                            <img src="<?php echo base_url('assets/admin/images/brands/github.png');?>" alt="Github">
+                                            <span><?php echo translate('testimonial') ?></span>
+                                        </a>
+                                    </div>
+                                <?php endif; ?>
+                                <div class="col-md-4">
+                                    <a class="dropdown-icon-item" href="<?php echo base_url('admin/slider'); ?>">
+                                        <img src="<?php echo base_url('assets/admin/images/brands/bitbucket.png');?>" alt="bitbucket">
+                                        <span><?php echo translate('gallery_image') ?></span>
+                                    </a>
+                                </div>
+                                <div class="col-md-4">
+                                    <a class="dropdown-icon-item" href="<?php echo base_url('admin/faq'); ?>">
+                                        <img src="<?php echo base_url('assets/admin/images/brands/dribbble.png');?>" alt="dribbble">
+                                        <span><?php echo translate('faqs'); ?></span>
                                     </a>
                                 </div>
                             </div>
 
-                            <div class="row g-0">
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
-                                        <img src="<?php echo base_url('assets/admin/images/brands/dropbox.png');?>" alt="dropbox">
-                                        <span>Dropbox</span>
-                                    </a>
-                                </div>
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
-                                        <img src="<?php echo base_url('assets/admin/images/brands/mail_chimp.png');?>" alt="mail_chimp">
-                                        <span>Mail Chimp</span>
-                                    </a>
-                                </div>
-                                <div class="col">
-                                    <a class="dropdown-icon-item" href="#">
-                                        <img src="<?php echo base_url('assets/admin/images/brands/slack.png');?>" alt="slack">
-                                        <span>Slack</span>
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="dropdown d-inline-block">
-                    <button type="button" class="btn header-item noti-icon position-relative" id="page-header-notifications-dropdown"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i data-feather="bell" class="icon-lg"></i>
-                        <span class="badge bg-danger rounded-pill">5</span>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0"
-                         aria-labelledby="page-header-notifications-dropdown">
-                        <div class="p-3">
-                            <div class="row align-items-center">
-                                <div class="col">
-                                    <h6 class="m-0"> Notifications </h6>
-                                </div>
-                                <div class="col-auto">
-                                    <a href="#!" class="small text-reset text-decoration-underline"> Unread (3)</a>
-                                </div>
-                            </div>
-                        </div>
-                        <div data-simplebar style="max-height: 230px;">
-                            <a href="#!" class="text-reset notification-item">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0 me-3">
-                                        <img src="<?php echo base_url('assets/admin/images/users/avatar-3.jpg');?>" class="rounded-circle avatar-sm" alt="user-pic">
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <h6 class="mb-1">James Lemire</h6>
-                                        <div class="font-size-13 text-muted">
-                                            <p class="mb-1">It will seem like simplified English.</p>
-                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>1 hours ago</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="text-reset notification-item">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0 avatar-sm me-3">
-                                        <span class="avatar-title bg-primary rounded-circle font-size-16">
-                                            <i class="bx bx-cart"></i>
-                                        </span>
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <h6 class="mb-1">Your order is placed</h6>
-                                        <div class="font-size-13 text-muted">
-                                            <p class="mb-1">If several languages coalesce the grammar</p>
-                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>3 min ago</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                            <a href="#!" class="text-reset notification-item">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0 avatar-sm me-3">
-                                        <span class="avatar-title bg-success rounded-circle font-size-16">
-                                            <i class="bx bx-badge-check"></i>
-                                        </span>
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <h6 class="mb-1">Your item is shipped</h6>
-                                        <div class="font-size-13 text-muted">
-                                            <p class="mb-1">If several languages coalesce the grammar</p>
-                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>3 min ago</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-
-                            <a href="#!" class="text-reset notification-item">
-                                <div class="d-flex">
-                                    <div class="flex-shrink-0 me-3">
-                                        <img src="<?php echo base_url('assets/admin/images/users/avatar-6.jpg');?>" class="rounded-circle avatar-sm" alt="user-pic">
-                                    </div>
-                                    <div class="flex-grow-1">
-                                        <h6 class="mb-1">Salena Layfield</h6>
-                                        <div class="font-size-13 text-muted">
-                                            <p class="mb-1">As a skeptical Cambridge friend of mine occidental.</p>
-                                            <p class="mb-0"><i class="mdi mdi-clock-outline"></i> <span>1 hours ago</span></p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                        <div class="p-2 border-top d-grid">
-                            <a class="btn btn-sm btn-link font-size-14 text-center" href="javascript:void(0)">
-                                <i class="mdi mdi-arrow-right-circle me-1"></i> <span>View More..</span>
-                            </a>
                         </div>
                     </div>
                 </div>
@@ -384,24 +296,6 @@
                             </ul>
                         </li>
                     <?php endif; ?>
-
-                    <li>
-                        <a href="javascript: void(0);" class="has-arrow">
-                            <i data-feather="layers"></i>
-                            <span data-key="t-tasks"><?php echo translate('master'); ?></span>
-                        </a>
-                        <ul class="sub-menu" aria-expanded="false">
-                            <li><a href="<?php echo base_url('admin/currency'); ?>" data-key="t-user-grid"><?php echo translate('currency'); ?></a></li>
-                            <li><a href="<?php echo base_url('admin/city'); ?>" data-key="t-user-list"><?php echo translate('city'); ?></a></li>
-                            <li><a href="<?php echo base_url('admin/location'); ?>" data-key="t-user-list"><?php echo translate('location'); ?></a></li>
-                            <?php if (get_site_setting('enable_testimonial') == 'Y'): ?>
-                                <li><a href="<?php echo base_url('admin/testimonial'); ?>" data-key="t-user-list"><?php echo translate('testimonial'); ?></a></li>
-                            <?php endif; ?>
-                            <li><a href="<?php echo base_url('admin/slider'); ?>" data-key="t-user-list"><?php echo translate('gallery_image'); ?></a></li>
-                            <li><a href="<?php echo base_url('admin/faq'); ?>" data-key="t-user-list"><?php echo translate('faqs'); ?></a></li>
-                        </ul>
-                    </li>
-
                 </ul>
             </div>
             <!-- Sidebar -->
@@ -415,6 +309,4 @@
     <!-- Start right Content here -->
     <!-- ============================================================== -->
     <div class="main-content">
-
         <div class="page-content">
-
