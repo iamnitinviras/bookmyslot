8889d8f014d305d2a126c7ba13d6bd1df75f8b33
Nitin Viras <nitinboricha91@gmail.com>
2022-12-16 16:24:22 +0530

    update

--- app/views/admin/language/add_update.php
+++ app/views/admin/language/add_update.php
@@ -4,70 +4,83 @@ $title = (set_value("title")) ? set_value("title") : (!empty($language_data) ? $
 $status = (set_value("status")) ? set_value("status") : (!empty($language_data) ? $language_data['status'] : '');
 $id = (set_value("id")) ? set_value("id") : (!empty($language_data) ? $language_data['id'] : 0);
 ?>
-<div class="dashboard-body">
-    <!-- Start Content -->
-    <div class="content">
-        <!-- Start Container -->
-        <div class="container-fluid">
-            <section class="form-light px-2 sm-margin-b-20 ">
-                <?php $this->load->view('message'); ?>
-
-                <div class="card mt-4">
-                    <div class="card-header">
-                        <h5 class="black-text font-bold mb-0">
-                            <?php echo isset($id) && $id > 0 ? translate('update') : translate('add'); ?> <?php echo translate('language'); ?>
-                        </h5>
-                    </div>
-                    <div class="card-body resp_mx-0">
-                        <?php
-                        echo form_open_multipart('admin/save-language', array('name' => 'LanguageForm', 'id' => 'LanguageForm'));
-                        echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $id));
-                        ?>
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="form-group">
-                                    <label for="name"> <?php echo translate('title'); ?><small class="required">*</small></label>
-                                    <input type="text" autocomplete="off" id="title" name="title" required="" value="<?php echo $title; ?>" class="form-control" placeholder="<?php echo translate('title'); ?>">                                    
-                                    <?php echo form_error('title'); ?>
-                                </div>
-                            </div>
-
-                            <div class="col-md-4">
-                                <label> <?php echo translate('status'); ?> <small class="required">*</small></label>
-                                <div class="form-inline">
-                                    <?php
-                                    $active = $inactive = '';
-                                    if ($status == "I") {
-                                        $inactive = "checked";
-                                    } else {
-                                        $active = "checked";
-                                    }
-                                    ?>
-                                    <div class="form-group">
-                                        <input name='status' value="A" type='radio' id='active'   <?php echo $active; ?>>
-                                        <label for="active"><?php echo translate('active'); ?></label>
-                                    </div>
-                                    <div class="form-group">
-                                        <input name='status' type='radio'  value='I' id='inactive'  <?php echo $inactive; ?>>
-                                        <label for='inactive'><?php echo translate('inactive'); ?></label>
-                                    </div>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-6">
+                            <h4 class="card-title"><?php echo translate('language'); ?></h4>
+                            <div class="page-title-box pb-0 d-sm-flex">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>"><?php echo translate('dashboard'); ?></a></li>
+                                        <li class="breadcrumb-item active"><?php echo translate('language'); ?></li>
+                                        <li class="breadcrumb-item active"><?php echo isset($id) && $id > 0 ? translate('update') : translate('add'); ?> <?php echo translate('language'); ?></li>
+                                    </ol>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="form-group mb-0">
-                            <button type="submit" class="btn btn-success waves-effect"><?php echo translate('save'); ?></button>
-                            <a href="<?php echo base_url('admin/manage-language'); ?>" class="btn btn-info waves-effect"><?php echo translate('cancel'); ?></a>
-                        </div>
-                        <?php echo form_close(); ?>
                     </div>
-                    <!--/Form with header-->
                 </div>
-                <!--Card-->       
-            </section>
-            <!-- End Login-->
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 m-auto">
+                            <?php $this->load->view('message'); ?>
+                            <?php
+                            echo form_open_multipart('admin/save-language', array('name' => 'LanguageForm', 'id' => 'LanguageForm'));
+                            echo form_input(array('type' => 'hidden', 'name' => 'id', 'id' => 'id', 'value' => $id));
+                            ?>
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <div class="form-group">
+                                                <label for="name"> <?php echo translate('title'); ?><small class="required">*</small></label>
+                                                <input type="text" autocomplete="off" id="title" name="title" required="" value="<?php echo $title; ?>" class="form-control" placeholder="<?php echo translate('title'); ?>">
+                                                <?php echo form_error('title'); ?>
+                                            </div>
+                                        </div>
+
+                                        <div class="col-md-4">
+                                            <label> <?php echo translate('status'); ?> <small class="required">*</small></label>
+                                            <div class="form-inline">
+                                                <?php
+                                                $active = $inactive = '';
+                                                if ($status == "I") {
+                                                    $inactive = "checked";
+                                                } else {
+                                                    $active = "checked";
+                                                }
+                                                ?>
+                                                <div class="form-group">
+                                                    <input name='status' value="A" type='radio' id='active'   <?php echo $active; ?>>
+                                                    <label for="active"><?php echo translate('active'); ?></label>
+                                                </div>
+                                                <div class="form-group">
+                                                    <input name='status' type='radio'  value='I' id='inactive'  <?php echo $inactive; ?>>
+                                                    <label for='inactive'><?php echo translate('inactive'); ?></label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card-footer">
+                                    <div class="form-group mb-0">
+                                        <button type="submit" class="btn btn-success waves-effect"><?php echo translate('save'); ?></button>
+                                        <a href="<?php echo base_url('admin/manage-language'); ?>" class="btn btn-info waves-effect"><?php echo translate('cancel'); ?></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <?php echo form_close(); ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
+    <!-- end row -->
 </div>
 <script src="<?php echo $this->config->item('js_url'); ?>module/language.js" type="text/javascript"></script>
 <?php
---
+++ info.php
@@ -0,0 +1,40 @@
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col-md-6 col-xl-6">
+                            <h4 class="card-title"><?php echo translate('city'); ?></h4>
+                            <div class="page-title-box pb-0 d-sm-flex">
+                                <div class="page-title-right">
+                                    <ol class="breadcrumb m-0">
+                                        <li class="breadcrumb-item"><a href="<?php echo base_url('admin/dashboard'); ?>"><?php echo translate('dashboard'); ?></a></li>
+                                        <li class="breadcrumb-item active"><?php echo translate('city'); ?></li>
+                                    </ol>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-xl-6 text-end add-new-btn-parent">
+                            <div class="text-sm-end">
+                                <a href='<?php echo base_url($folder_name.'/add-city'); ?>'class="btn btn-success btn-rounded waves-effect waves-light mb-2 me-2"><i class="mdi mdi-plus me-1"></i> <?php echo translate('add'); ?> <?php echo translate('city'); ?></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-12 m-auto">
+                            <?php $this->load->view('message'); ?>
+
+                            <div class="table-responsive">
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end row -->
+</div>
